6a7526cb28bc022552dda1fb0d7b20dfce78b187
YunkangTao <1461932145@qq.com>
2025-02-06 11:41:21 +0800

    update

--- chat.py
+++ chat.py
@@ -2,25 +2,25 @@ import torch
 from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
 from qwen_vl_utils import process_vision_info
 
-# # default: Load the model on the available device(s)
-model = Qwen2_5_VLForConditionalGeneration.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct", torch_dtype="auto", device_map="auto")
+# default: Load the model on the available device(s)
+# model = Qwen2_5_VLForConditionalGeneration.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct", torch_dtype="auto", device_map="auto")
 
 # We recommend enabling flash_attention_2 for better acceleration and memory saving, especially in multi-image and video scenarios.
-# model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-#     "Qwen/Qwen2.5-VL-7B-Instruct",
-#     torch_dtype=torch.bfloat16,
-#     attn_implementation="flash_attention_2",
-#     device_map="auto",
-# )
+model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+    "Qwen/Qwen2.5-VL-7B-Instruct",
+    torch_dtype=torch.bfloat16,
+    attn_implementation="flash_attention_2",
+    device_map="auto",
+)
 
 # default processor
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct")
+# processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct")
 
 # The default range for the number of visual tokens per image in the model is 4-16384.
 # You can set min_pixels and max_pixels according to your needs, such as a token range of 256-1280, to balance performance and cost.
-# min_pixels = 256*28*28
-# max_pixels = 1280*28*28
-# processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
+min_pixels = 256 * 28 * 28
+max_pixels = 1280 * 28 * 28
+processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct", min_pixels=min_pixels, max_pixels=max_pixels)
 
 messages = [
     {
@@ -28,9 +28,12 @@ messages = [
         "content": [
             {
                 "type": "image",
-                "image": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg",
+                "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/image.png",
             },
-            {"type": "text", "text": "Describe this image."},
+            {
+                "type": "text",
+                "text": "Please carefully understand these images and help me find the image for first person shooter game screenshots. This type of image simulates holding a gun or other weapon from a first person perspective. Just list it without explaining the reason.",
+            },
         ],
     }
 ]
--- multi_image.py
+++ multi_image.py
@@ -3,15 +3,15 @@ from transformers import Qwen2_5_VLForConditionalGeneration, AutoProcessor
 from qwen_vl_utils import process_vision_info
 
 # default: Load the model on the available device(s)
-model = Qwen2_5_VLForConditionalGeneration.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct", torch_dtype="auto", device_map="auto")
+# model = Qwen2_5_VLForConditionalGeneration.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct", torch_dtype="auto", device_map="auto")
 
 # We recommend enabling flash_attention_2 for better acceleration and memory saving, especially in multi-image and video scenarios.
-# model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-#     "Qwen/Qwen2.5-VL-7B-Instruct",
-#     torch_dtype=torch.bfloat16,
-#     attn_implementation="flash_attention_2",
-#     device_map="auto",
-# )
+model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+    "Qwen/Qwen2.5-VL-7B-Instruct",
+    torch_dtype=torch.bfloat16,
+    attn_implementation="flash_attention_2",
+    device_map="auto",
+)
 
 # default processor
 # processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-7B-Instruct")
@@ -27,14 +27,29 @@ messages = [
     {
         "role": "user",
         "content": [
-            {"type": "image", "image": "/mnt2/lei/RetrievalVLM/20250114_find_20_cases/case13_find_FPS_game/ImagePoolFolder/1.png"},
-            {"type": "image", "image": "/mnt2/lei/RetrievalVLM/20250114_find_20_cases/case13_find_FPS_game/ImagePoolFolder/2.png"},
-            {"type": "image", "image": "/mnt2/lei/RetrievalVLM/20250114_find_20_cases/case13_find_FPS_game/ImagePoolFolder/3.png"},
-            {"type": "image", "image": "/mnt2/lei/RetrievalVLM/20250114_find_20_cases/case13_find_FPS_game/ImagePoolFolder/4.png"},
-            {"type": "image", "image": "/mnt2/lei/RetrievalVLM/20250114_find_20_cases/case13_find_FPS_game/ImagePoolFolder/5.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/1.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/2.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/3.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/4.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/5.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/6.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/7.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/8.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/9.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/10.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/11.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/12.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/13.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/14.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/15.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/16.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/17.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/18.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/19.png"},
+            {"type": "image", "image": "/mnt2/lei/Qwen2.5-VL/FPS_case/ImagePoolFolder/20.png"},
             {
                 "type": "text",
-                "text": "Please help me find the image index of screenshot of First Person Shooter Game from the ImagePoolFolder. This type of image simulates holding a gun or other weapon from a first person perspective.",
+                "text": "Please carefully understand these images and help me find the image for first person shooter game screenshots. This type of image simulates holding a gun or other weapon from a first person perspective. Just list it without explaining the reason.",
             },
         ],
     }
